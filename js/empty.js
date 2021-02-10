f9b6ee3f06500e26f29c17bf0c4f3aa822387ae5
Glinko <example.gmail.com>
2021-02-11 02:37:20 +0300

    404

--- js/empty.js
+++ js/empty.js
@@ -2,6 +2,6 @@ const menuEmpBtn = document.querySelector(".empty-menu__icon"),
   EmpMenu = document.querySelector(".empty-menu-content");
 
 menuEmpBtn.addEventListener("click", () => {
-  menuEmpBtn.classList.toggle("menu-btn__active");
+  menuEmpBtn.classList.toggle("empty-btn__active");
   EmpMenu.classList.toggle("empty-menu-content__active");
 });
